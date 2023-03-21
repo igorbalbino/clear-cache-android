88e077115fd9c63f529c4c43be623ef9be2854ff
Igor do Espírito Santo Balbino <igor.balbino@teknisa.com>
2023-03-20 23:28:34 -0300

    adicionando codigo para limpar cache usando comando do console do android.

--- CrearCache/app/src/main/java/br/com/igor/crearcache/MainActivity.java
+++ CrearCache/app/src/main/java/br/com/igor/crearcache/MainActivity.java
@@ -2,22 +2,21 @@ package br.com.igor.crearcache;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = " | MainActivity | ";
+    private static final long  MEGABYTE = 1024L * 1024L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +32,10 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "O cache dos seus pps estão sendo limpos...", Toast.LENGTH_SHORT)
-                        .show();
+                Toast.makeText(MainActivity.this, "O cache dos seus apps serão limpos...",
+                        Toast.LENGTH_SHORT)
+                .show();
+
                 PackageManager pm = getPackageManager();
                 List<ApplicationInfo> apps = pm.getInstalledApplications(0);
                 clearCache(apps);
@@ -44,25 +45,67 @@ public class MainActivity extends AppCompatActivity {
 
     private void clearCache(List<ApplicationInfo> apps) {
         PackageManager pm = getPackageManager();
-        List<ApplicationInfo> cloneApps = apps;
+        double totalCache = 0;
         try {
             for (ApplicationInfo app : apps) {
-                // Retrieve information about the app, such as its name, package name, etc.
                 String appName = pm.getApplicationLabel(app).toString();
                 String packageName = app.packageName;
                 int versionCode = pm.getPackageInfo(packageName, 0).versionCode;
 
-                // Do something with the app information
-                Log.d(TAG, "App name: " + appName + ", package name: " + packageName + ", version code: " + versionCode);
-                // remove primeira posição da lista, pq o app ja foi limpo
-                cloneApps.remove(0);
+                byte[] bCacheSize = getCacheSize(app.packageName);
+                double megabytes = (double) bCacheSize.length / 1000000.0;
+                totalCache += megabytes;
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        clear(app.packageName);
+                    }
+                });
+
+                Log.d(TAG, "App name: " + appName + ", package name: " + packageName +
+                        ", version code: " + versionCode + ", cache size(bytes): " + bCacheSize +
+                        "\n");
             }
-            ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-            activityManager.clearApplicationUserData();
-        } catch(Exception e) {
-            Log.e(TAG, "Error: PackageManager.NameNotFoundException.\n" + e.getMessage());
-            apps = null;
-            clearCache(cloneApps);
+            Toast.makeText(this, "Cache dos apps apagado!" +
+                "\nTotal de cache apagado: " + totalCache,
+                    Toast.LENGTH_SHORT)
+            .show();
+            Log.i(TAG, "Total de cache apagado: " + totalCache);
+
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Error PackageManager.NameNotFoundException: " + e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private byte[] getCacheSize(String packageName) {
+        try {
+            return getApplicationContext().getPackageManager()
+                    .getApplicationInfo(packageName, PackageManager.GET_META_DATA).dataDir.getBytes();
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e(TAG, "Erro ao obter o tamanho do cache do aplicativo " + packageName, e);
+            return "".getBytes();
+        }
+    }
+
+    private void clear(String packageName) {
+        try {
+            getApplicationContext().deleteFile(packageName);
+            Runtime runtime = Runtime.getRuntime();
+            String command = "pm clear " + packageName;
+            try {
+                Process process = runtime.exec(command);
+                process.waitFor();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "Erro ao limpar o cache do aplicativo " + packageName, e);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Log.e(TAG, "Erro ao limpar o cache do aplicativo " + packageName, e);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Erro ao limpar o cache do aplicativo " + packageName, e);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Cache Cleaner Android
 
 App feito em android com Android SDK mínimo 21 e Android SDK recomendado 32.
-O app foi feito usando funções padrões do android para limpar cache do próprio e de outros aplicativos instalados no smartphone.
+O app foi feito usando comandos do console do android para limpar cache do próprio e de outros aplicativos instalados no smartphone.
 Alguns apps podem não ser limpos pois dependendo do caso é necessário root no android para poder acesssar as configurações de alguns apps.
 
 Após limpar o cache dos apps, pode ocorrer piora ou melhora na performance de alguns apps. A melhora pode ocorrer pois o aparelho estará limpo então será dado novo início ao cache, podendo o app baixar um cache atualiado com melhores dados.
